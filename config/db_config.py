5710d2ea856b11369a8b1284d6706d52ed2db4aa
Surya Pratap Mallick <pratapsurya3024@gmail.com>
2026-04-15 15:02:50 +0530

    Day-2 timescaledb setup complete

---
+++ config/db_config.py
@@ -0,0 +1,12 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'predictive_maintenance',
+    'user': 'postgres',
+    'password': 'surya@2006'
+}
+
+CONNECTION_STRING = (
+    "postgresql://postgres:surya@2006"
+    "@localhost:5432/predictive_maintenance"
+)
